fdee631f5e1e694be95e5fc6ff9c452e278df1e3
ARORA Mudit <mudit.arora@soprasteria.com>
2024-07-28 01:31:44 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,37 @@
+console.log("Hello from Javascript");
+console.log(x);
+
+// Day 1
+
+// Activity 1: Variable declaration
+var x = 10;
+let y = 10;
+const z = 10;
+console.log("var x is : " + x);
+console.log("let y is : " + y);
+console.log("const z is : " + z);
+
+// const declaration
+const a = true;
+console.log("const bool a is : " + a);
+
+// Activity 3: Data Types
+let n = 10;
+let s = "mudit";
+let b = true;
+let o = {
+  name: "Jonny",
+  age: 50,
+};
+let array = [1, 2, 3, 4, 5];
+
+console.log("Type of n :", typeof n);
+console.log("Type of s :", typeof s);
+console.log("Type of b :", typeof b);
+console.log("Type of o :", typeof o);
+console.log("Type of array :", typeof array);
+
+// Activity 4:  Reassi-ning Variables
+let nn = 10;
+nn = 20;
+console.log("VALUE of nn is:" + nn);
