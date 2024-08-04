09e13c679d7a6bbb429bbbf7e91541359f8f414d
ARORA Mudit <mudit.arora@soprasteria.com>
2024-08-05 01:10:39 +0530

    Closures Advantages

--- closures.js
+++ closures.js
@@ -22,3 +22,39 @@ function m() {
   n();
 }
 m();
+
+console.log("________04th August _Closures Advantages_____________");
+
+//example of closures fascilitating Encapsulation
+
+function outest() {
+  function outer(b) {
+    function inner() {
+      var c = 30;
+
+      console.log(b, a, c);
+    }
+    let a = 20;
+    return inner;
+  }
+  return outer;
+}
+var bc = outest()("Hello");
+bc();
+console.log("__Scalable and best code for encapsulation example ");
+
+function Counter() {
+  var count = 1;
+  this.incrementCounter = function () {
+    count++;
+    console.log(count);
+  };
+  this.decrementCounter = function () {
+    count--;
+    console.log(count);
+  };
+}
+
+var counter1 = new Counter();
+counter1.incrementCounter();
+counter1.decrementCounter();
